4b821f91104c303d4e9bee9fc4a4264a2f7212a8
Owain Kenway <o.kenway@ucl.ac.uk>
2026-04-10 15:06:36 +0100

    Python script to attach GPU to VM

---
+++ inference-host/attach_gpu.py
@@ -0,0 +1,33 @@
+import yaml
+import sys
+import subprocess
+
+GPU_DEVICE=[{'deviceName':GPU_STRING}]
+KUBECTL_LOCATION=subprocess.run(["/usr/bin/which", "kubectl"], capture_output=True, text=True).stdout.strip()
+
+if len(sys.argv < 3):
+	print("Run with name of VM to modify and host.")
+	sys.exit(1)
+
+
+
+vm=sys.argv[1]
+gname=sys.argv[2]
+
+GPU_STRING=subprocess.run([KUBECTL_LOCATION, "get", "pcidevice", gname,"-o",  "jsonpath='{.status.resourceName}'"], capture_output=True, text=True).stdout.strip().strip("'")
+
+
+
+GPU_DEVICE=[{'deviceName':GPU_STRING, 'name':gname}]
+
+orig_config_raw = subprocess.run([KUBECTL_LOCATION, "get", "vm", vm, "-o", "yaml"], capture_output=True, text=True).stdout.strip()
+orig_config=yaml.safe_load(orig_config_raw)
+
+orig_config["spec"]["template"]["spec"]["domain"]["devices"]["hostDevices"] = GPU_DEVICE
+
+new_config_raw = yaml.dump(orig_config, sort_keys=False, allow_unicode=True)
+
+with open("_temp.yaml", "w", encoding="utf-8") as f:
+	f.write(new_config_raw)
+
+subprocess.run([KUBECTL_LOCATION, "apply", "-f", "_temp.yaml"])
