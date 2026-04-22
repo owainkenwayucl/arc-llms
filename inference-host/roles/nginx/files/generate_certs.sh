f8770d40a617ca10901051c95841547d6036c640
Owain Kenway <o.kenway@ucl.ac.uk>
2026-04-22 10:36:30 +0100

    IT helps when I remember how to use things I wrote a year ago

--- inference-host/roles/nginx/files/generate_certs.sh
+++ inference-host/roles/nginx/files/generate_certs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 openssl req -x509 -newkey rsa:4096 -keyout /etc/nginx/nginx.key -out /etc/nginx/nginx.crt -sha256 -days 3650 -nodes -subj "/CN=${1}" -addext "subjectAltName = IP:${1}"
 chgrp nginx /etc/nginx/nginx.key
 chmod 640 /etc/nginx/nginx.key
