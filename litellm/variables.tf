ac912ac3762e60bb462f9daf39dc8511b77740ca
Owain Kenway <o.kenway@ucl.ac.uk>
2026-05-18 13:29:45 +0100

    Set up base docker based environment for litellm

---
+++ litellm/variables.tf
@@ -0,0 +1,19 @@
+variable img_display_name {
+  type = string
+  default = "almalinux_9.7"
+}
+
+variable username {
+  type = string
+  default = "uccaoke"
+}
+
+variable keyname {
+  type = string
+  default = "uccaoke-cnc"
+}
+
+variable vm_count {
+  type    = number
+  default = 3
+}
